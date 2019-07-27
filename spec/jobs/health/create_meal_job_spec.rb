7fe98a10c9ecbb64226720d5f56ba1921db95b9a
Alex Gascón <alexgascon.93@gmail.com>
2019-07-27 23:37:34 +0100

    Modifying SNS message processing to include correct structure
    The structure that we were assuming for the SNS events wasn't the correct one, because apart from the actual message there is also other metadata and information related with resources used (like the topic). In this commit we implement several changes to prepare our code for the actual payload format:
    
    - **Changing the fixtures used in the tests**: this allows us to know if the rest of the code is correct or not
    - **Parsing the JSON fixture in the tests**: the `event` object that our handlers receive is not a string as we expected, but a hash. Therefore, our tests will need to receive a hash too.
    - **Modifying the parser**: changing the parsing logic to get to the actual message payload. It is important to note that `Records` can be an array, but as we expect our messages to get to us one by one, we'll assume that it's fine to always get the first (as it should be the only one)

--- spec/jobs/health/create_meal_job_spec.rb
+++ spec/jobs/health/create_meal_job_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe Health::CreateMealJob do
   describe 'create_meal' do
-    let(:event) { File.read('spec/fixtures/sns_events/health/MealEaten.json') }
+    let(:event) { JSON.parse(File.read('spec/fixtures/sns_events/health/MealEaten.json')) }
 
     subject { described_class.perform_now(:create_meal, event) }
 
@@ -23,7 +23,7 @@ RSpec.describe Health::CreateMealJob do
     end
 
     context 'when the event does not have any date' do
-      let(:event) { File.read('spec/fixtures/sns_events/health/MealEaten-no_date.json') }
+      let(:event) { JSON.parse(File.read('spec/fixtures/sns_events/health/MealEaten-no_date.json')) }
 
       it 'creates a new meal' do
         expect { subject }.to change { Health::Meal.all.count }.by(1)
