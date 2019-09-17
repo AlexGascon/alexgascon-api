66c8c1c6eca501c02c468ab958faa3285bd2205e
Alex Gascón <alexgascon.93@gmail.com>
2019-09-17 02:02:16 +0100

    Making AuthToken a Singleton

--- app/models/partners/dexcom/auth_token.rb
+++ app/models/partners/dexcom/auth_token.rb
@@ -5,14 +5,15 @@ module Partners
     class AuthToken
       include Dynamoid::Document
 
-      field :account_id, :string
       field :access_token, :string
       field :expiration_time, :number 
       field :refresh_token, :string
-      
-      validates_presence_of :account_id
-      validates_presence_of :access_token
-      validates_presence_of :refresh_token
+
+      def self.instance
+        return new if count.zero?
+
+        first
+      end
     end
   end
 end
--- spec/models/partners/dexcom/auth_token_spec.rb
+++ spec/models/partners/dexcom/auth_token_spec.rb
@@ -1,49 +1,35 @@
 # frozen_string_literal: true
 
 RSpec.describe Partners::Dexcom::AuthToken do
-  describe 'validations' do
-    it 'marks the record as valid if all the fields are present' do
-      token = described_class.new(
-        account_id: "account_id",
-        access_token: "access_token",
-        expiration_time: 1234,
-        refresh_token: "refresh_token"
-      )
+  describe '.instance' do
+    context 'if the token has not been created yet' do
+      before do
+        described_class.delete_table
+        described_class.create_table
+      end
 
-      expect(token).to be_valid
+      it 'creates a new token' do
+        expect { described_class.instance.save }.to change { described_class.count }.by(1)
+      end
     end
 
-    it 'validates presence of account_id' do
-      token = described_class.new(
-        account_id: "",
-        access_token: "access_token",
-        expiration_time: 1234,
-        refresh_token: "refresh_token"
-      )
+    context 'if a token already exists' do
+      before { described_class.instance.save }
 
-      expect(token).not_to be_valid
+      it 'always returns the same instance' do
+        expect(described_class.instance.id).to eq(described_class.instance.id)
+      end
+
+      it 'does not create new tokens' do
+        expect { described_class.instance.save }.not_to change { described_class.count }
+      end
     end
+  end
 
-    it 'validates presence of access_token' do
-      token = described_class.new(
-        account_id: "account_id",
-        access_token: "",
-        expiration_time: 1234,
-        refresh_token: "refresh_token"
-      )
-
-      expect(token).not_to be_valid
-    end
-
-    it 'validates presence of refresh_token' do
-      token = described_class.new(
-        account_id: "account_id",
-        access_token: "access_token",
-        expiration_time: 1234,
-        refresh_token: ""
-      )
-
-      expect(token).not_to be_valid
+  describe '.new' do
+    it 'is marked as private' do
+      skip 'Dynamoid calls .new when doing first, last and other methods; look for a workaround'
+      expect { described_class.new }.to raise_error(NoMethodError)
     end
   end
 end
