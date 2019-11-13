7936128fed0956cff035e95d142f825fa3166464
Alex Gascón <alexgascon.93@gmail.com>
2019-11-13 07:49:45 +0000

    Publishing metrics after creating the injection
    Adding extra logic in CreateInjectionJob to publish the injection metric
    after we create it.
    
    We have also done some re-ordering in the lines to make it clearer how
    it works: the sns_event triggers the create injection method (as it's
    the first one that goes after it). We have removed the blank line
    between them to make this more obvious

--- app/jobs/health/create_injection_job.rb
+++ app/jobs/health/create_injection_job.rb
@@ -2,16 +2,24 @@
 
 module Health
   class CreateInjectionJob < ::ApplicationJob
-    sns_event 'InsulinInjected'
 
+    sns_event 'InsulinInjected'
     def create_injection
       injection_information = Health::InjectionParser.new(event).parse
 
-      Health::Injection.create!(
+      injection = Health::Injection.create!(
         units: injection_information[:units],
         injection_type: injection_information[:injection_type],
         notes: injection_information[:notes]
       )
+
+      publish_injection_metric(injection)
+    end
+
+    private
+
+    def publish_injection_metric(injection)
+      AwsServices::CloudwatchWrapper.new.publish_injection(injection)
     end
   end
 end
