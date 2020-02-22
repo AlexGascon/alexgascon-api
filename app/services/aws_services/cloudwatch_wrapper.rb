83acfa557a236bea1e81dbbbb0f29c287963cc4a
Alex Gascón <alexgascon.93@gmail.com>
2020-02-22 18:50:14 +0000

    Retrieving the image for last 24h insulin
    Adding a new method to CloudwatchWrapper,
    `retrieve_insulin_last_day_image`, that retrieves the metric widget
    image, uploads it to S3 and returns its URL

--- app/services/aws_services/cloudwatch_wrapper.rb
+++ app/services/aws_services/cloudwatch_wrapper.rb
@@ -4,6 +4,9 @@ module AwsServices
   class CloudwatchWrapper
     attr_reader :client
 
+    METRIC_WIDGET_SOURCES_PATH = 'app/resources/json/aws/metric_widget_sources'
+    WIDGET_LAST_DAY = 'insulin-last-day'
+
     def initialize
       @client = Aws::CloudWatch::Client.new
     end
@@ -16,6 +19,28 @@ module AwsServices
     def publish_expense(expense)
       metric_data = Metrics::ExpenseMetric.new(expense)
       client.put_metric_data(namespace: 'Finance', metric_data: [metric_data])
+    end
+
+    def retrieve_insulin_last_day_image
+      metric_widget = get_metric_widget_image WIDGET_LAST_DAY
+      s3.store_image(metric_widget)
+    end
+
+    private
+
+    def s3
+      @s3 ||= AwsServices::S3Wrapper.new
+    end
+
+    def telegram
+      @telegram ||= TelegramBot.new
+    end
+
+    def get_metric_widget_image(widget_source_name)
+      metric_widget_source = File.read "#{METRIC_WIDGET_SOURCES_PATH}/#{widget_source_name}.json"
+      response = client.get_metric_widget_image(metric_widget: metric_widget_source)
+
+      response['metric_widget_image']
     end
   end
 end
