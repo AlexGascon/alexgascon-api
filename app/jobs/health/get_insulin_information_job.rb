2ad2d27c6921ce968373ff045f111e9aa974c657
Alex Gascón <alexgascon.93@gmail.com>
2020-03-05 23:04:09 +0000

    Sending a preparation message before sending the insulin summary
    As generating an insulin image and sending it via Telegram can be a slow
    process, we'll add a message at the start to let the user know that the
    request was received correctly and that we are preparing the information

--- app/jobs/health/get_insulin_information_job.rb
+++ app/jobs/health/get_insulin_information_job.rb
@@ -5,6 +5,7 @@ module Health
 
     sns_event 'SummaryRequested'
     def get_insulin_information
+      telegram.send_message('Preparing insulin summary...')
       image_url = cloudwatch.retrieve_insulin_last_day_image
       telegram.send_photo(image_url)
     end
