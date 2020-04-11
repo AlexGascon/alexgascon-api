6c9a53045b4e468e5099c137343fe12d1faf9b8f
Alex Gascón <alexgascon.93@gmail.com>
2020-04-11 17:48:49 +0200

    Adding CloudWatch Alarms to monitor correct metrics publication
    Creating a CloudFormation stack with CW Alarms that we'll use to monitor
    the correct publication of metrics in our app.
    
    We'll be verifying that, for every datapoint that we publish periodically
    (the two types of insulin and the 4 types of expense), there's at least
    one datapoint per hour. For insulin we'll have a different alarm for
    each type, while for expenses we'll have a single alarm for all of them
    
    For the moment, the alarms are not associated to any action. We'll add
    that later

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -3,3 +3,9 @@ bundle exec jets deploy production
 
 echo "Creating the new DynamoDB tables..."
 JETS_ENV=production bundle exec rake dynamoid:create_tables
+
+echo "Updating the CloudFormation stacks..."
+aws cloudformation deploy \
+	--template-file infrastructure/alarms.yml \
+	--stack-name alexgascon-api-Alarms \
+	--no-fail-on-empty-changeset
