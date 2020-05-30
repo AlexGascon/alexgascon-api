1dd8821c788c1f68da5ea708846844e6096e725d
Alex Gascón <alexgascon.93@gmail.com>
2020-05-30 09:59:41 +0200

    Minor fixes

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,5 +1,5 @@
-RSpec.shared_examples 'retry metric' do |retries| let(:expected_retries) { retries }
-  let (:expected_retries) { retries }
+RSpec.shared_examples 'retry metric' do |retries|
+  let(:expected_retries) { retries }
 
   it 'publishes a metric with the retries' do
     expect(PublishCloudwatchDataCommand).to receive(:new).with(retries_metric)
