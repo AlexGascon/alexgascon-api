5afb4e3203a38b51148cecf88838856c094b6f77
Alex Gascón <alexgascon.93@gmail.com>
2020-10-20 09:32:18 +0100

    Changing the alert message when an alarm transitions to OK
    Changing AlarmReceivedJob to use a different message header when the
    alarm transitions to OK state. This will allow users to easily
    differentiate between messages where the alarm fires and when it gets
    back to normal, specially when only the start of the message can be seen
    (e.g. on a push notification)

--- spec/jobs/alarm_received_job_spec.rb
+++ spec/jobs/alarm_received_job_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe AlarmReceivedJob do
   describe 'run' do
     before { stub_command(SendTelegramMessageCommand) }
 
-    let(:event) { load_json_fixture('sns_events/cloudwatch_alarm') }
+    let(:event) { load_json_fixture('sns_events/cloudwatch_alarm_alarm') }
 
     it 'sends a Telegram message' do
       expect_any_instance_of(SendTelegramMessageCommand).to receive(:execute)
@@ -14,19 +14,42 @@ RSpec.describe AlarmReceivedJob do
       subject
     end
 
-    it 'passes the correct message to the command' do
-      expected_message = <<~MSG
-      \xF0\x9F\x94\x94 Alarm! Alarm! \xF0\x9F\x94\x94
+    context 'when the alarm has changed to ALARM state' do
+      let(:event) { load_json_fixture('sns_events/cloudwatch_alarm_alarm') }
 
-      The alarm "Example alarm name" has changed to "ALARM"
+      it 'passes an alarm message to the command' do
+        expected_message = <<~MSG
+        \xF0\x9F\x94\x94 Alarm! Alarm! \xF0\x9F\x94\x94
 
-      Reason:
-      Threshold Crossed: 1 datapoint (10.0) was greater than or equal to the threshold (1.0).
-      MSG
+        The alarm "Example alarm name" has changed to "ALARM"
 
-      expect(SendTelegramMessageCommand).to receive(:new).with(expected_message)
+        Reason:
+        Threshold Crossed: 1 datapoint (10.0) was greater than or equal to the threshold (1.0).
+        MSG
 
-      subject
+        expect(SendTelegramMessageCommand).to receive(:new).with(expected_message)
+
+        subject
+      end
+    end
+
+    context 'when the alarm has changed to OK state' do
+      let(:event) { load_json_fixture('sns_events/cloudwatch_alarm_ok') }
+
+      it 'passes an OK message to the command' do
+        expected_message = <<~MSG
+        \xE2\x9C\x85 All good! \xE2\x9C\x85
+
+        The alarm "Example alarm name" has changed to "OK"
+
+        Reason:
+        Threshold Crossed: 1 datapoint (0.5) was not greater than or equal to the threshold (1.0).
+        MSG
+
+        expect(SendTelegramMessageCommand).to receive(:new).with(expected_message)
+
+        subject
+      end
     end
   end
 end
