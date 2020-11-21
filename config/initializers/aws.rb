cc853293f29919d250b206332ea9d3b30a33b521
Alex Gascón <alexgascon.93@gmail.com>
2020-11-21 22:53:11 +0000

    Avoiding to specify the AWS region for DynamoDB

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Aws.config.update(
-  region: ENV['AWS_REGION'],
+  region: 'eu-west-1',
   credentials: Aws::Credentials.new(ENV['AWS_ACCESS'], ENV['AWS_SECRET'])
 )
