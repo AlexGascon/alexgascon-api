846e1bbc95d58d19529a326ae1e540c67748f178
Alex Gascón <alexgascon.93@gmail.com>
2021-04-24 10:28:35 +0100

    Removing Bankia
    The Bankia bank account isn't in use anymore, so we don't need to
    monitor it for new transactions. This commit removes all the now unused
    code from the project.

--- app/models/ynab/accounts.rb
+++ app/models/ynab/accounts.rb
@@ -3,14 +3,12 @@
 module Ynab
   module Accounts
     AIB = 'AIB'
-    BANKIA = 'Bankia'
     OPENBANK = 'Openbank'
 
     DEFAULT = AIB
 
     ACCOUNT_IDS = {
       AIB => '52b9fb9e-43fd-4610-a2be-7ced8c8c7fe4',
-      BANKIA => '110eb1a6-1d99-4117-8f15-a71a014fff64',
       OPENBANK => 'ee318949-69b9-42cd-9b07-1dfebf9bbd37'
     }.freeze
 
