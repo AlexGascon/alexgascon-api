557c4cce8e6dfc60bad95b25ec76640f1fc0802e
Alex Gascón <alexgascon.93@gmail.com>
2021-05-08 18:54:35 +0100

    Logging number of published datapoints
    Adding a log message in GetLiveDexcomDataJob to inform of the number of
    datapoints that are going to be published

--- app/jobs/health/get_live_dexcom_data_job.rb
+++ app/jobs/health/get_live_dexcom_data_job.rb
@@ -9,6 +9,7 @@ module Health
 
       Health::GlucoseValueFactory
         .from_dexcom_gem_entries(bgs)
+        .tap { |glucose_values| Jets.logger.info "Number of glucose values to publish: #{glucose_values.size}" }
         .each { |glucose_value| PublishCloudwatchDataCommand.new(glucose_value).execute }
     end
   end
