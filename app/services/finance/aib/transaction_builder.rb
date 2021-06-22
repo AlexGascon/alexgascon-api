dd0a69f5c15b2b0b46b70338acc8f75b31ec533a
Alex Gascón <alexgascon.93@gmail.com>
2021-06-22 07:46:48 +0100

    Fixing AIB transaction builder
    In the AIB transaction builder, we were extracting the information from
    the Plaid object using [] notation, as if we were working with a hash.
    However, after the migration to Plaid, we now get a Plaid::Transaction
    object instead. This commit fixes the bug and the tests

--- app/services/finance/aib/transaction_builder.rb
+++ app/services/finance/aib/transaction_builder.rb
@@ -6,7 +6,7 @@ module Finance
       private
 
       def amount_in_cents
-        -@transaction_information['amount'] * CENTS_PER_EURO
+        -@transaction_information.amount * CENTS_PER_EURO
       end
 
       def bank
@@ -14,15 +14,15 @@ module Finance
       end
 
       def internal_id
-        @transaction_information['transaction_id']
+        @transaction_information.transaction_id
       end
 
       def transaction_datetime
-        DateTime.parse(@transaction_information['date']).in_time_zone('Europe/Dublin').to_date
+        DateTime.parse(@transaction_information.date).in_time_zone('Europe/Dublin').to_date
       end
 
       def description
-        @transaction_information['name']
+        @transaction_information.name
       end
     end
   end
--- spec/services/finance/aib/transaction_builder_spec.rb
+++ spec/services/finance/aib/transaction_builder_spec.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.describe Finance::Aib::TransactionBuilder do
-  let(:transaction_information) { load_json_fixture 'finance/aib/transaction' }
+  let(:transaction_information) { build(:plaid_transaction) }
 
   describe '#build' do
     subject(:bank_transaction) { described_class.new(transaction_information).build }
 
     it 'sets the amount_in_cents' do
-      expect(bank_transaction.amount_in_cents).to eq -2000
+      expect(bank_transaction.amount_in_cents).to eq -1139
     end
 
     it 'sets the bank' do
@@ -15,23 +15,23 @@ RSpec.describe Finance::Aib::TransactionBuilder do
     end
 
     it 'sets the datetime' do
-      expect(bank_transaction.datetime).to eq DateTime.parse('2021-05-24')
+      expect(bank_transaction.datetime).to eq DateTime.parse('2021-06-11')
     end
 
     it 'sets the description' do
-      expect(bank_transaction.description).to eq 'ATM FAKE STREET 22MAY21 TIME 12:18'
+      expect(bank_transaction.description).to eq 'DELIVEROO'
     end
 
     it 'sets the internal_id' do
-      expect(bank_transaction.internal_id).to eq 'aJgKYrKa7Yt7oe3pb68qiX509M3gKgCjb9J4y'
+      expect(bank_transaction.internal_id).to eq 'W00Vi6WZnIL0J4I0ToP2uTjvb3GgCvjKmX4c3'
     end
 
     it 'sets year_month' do
-      expect(bank_transaction.year_month).to eq '2021-05'
+      expect(bank_transaction.year_month).to eq '2021-06'
     end
 
     it 'sets day' do
-      expect(bank_transaction.day).to eq '24'
+      expect(bank_transaction.day).to eq '11'
     end
   end
 end
